fd523dbee807e2bcafecaf299a7a8c7b1a0bb1c2
Jayaprakash Reddy Mula Chennaiah Gari <mcjayaprakashreddy8309@gmail.com>
2025-05-15 20:01:29 +0530

    Add files via upload

---
+++ task11/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Hello from './components/Function'
+
+function App() {
+  
+
+  return (
+    <>
+     <Hello/>
+    </>
+  )
+}
+
+export default App
